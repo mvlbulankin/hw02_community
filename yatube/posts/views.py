61cbed09fab4bcca99514bc57880152a178f6354
Михаил Буланкин <mvl.bulankin@gmail.com>
2022-08-25 09:18:40 +0300

    fix head_title in posts templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ from .models import Group, Post
 
 def index(request):
     template = "posts/index.html"
-    posts = Post.objects.select_related("author").prefetch_related("group")[
+    posts = Post.objects.select_related("author", "group")[
         : settings.NUM_OF_POSTS_ON_PAGE
     ]
     return render(request, template, {"posts": posts})
@@ -15,7 +15,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     template = "posts/group_list.html"
-    posts = group.posts.select_related("author").prefetch_related("group")[
+    posts = group.posts.select_related("author", "group")[
         : settings.NUM_OF_POSTS_ON_PAGE
     ]
     return render(request, template, {"group": group, "posts": posts})
